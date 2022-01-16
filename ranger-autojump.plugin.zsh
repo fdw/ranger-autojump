c1822eb65ceb436a39f9c627082e41adf97185cb
Charles N. Christensen <charles.n.chr@gmail.com>
2022-01-16 18:31:29 +0000

    Cleaned up

--- ranger-autojump.plugin.zsh
+++ ranger-autojump.plugin.zsh
@@ -1,2 +1,23 @@
-source ${0:A:h}/r_function.zsh
+# Add a `r` function to zsh that opens ranger either at the given directory or
+# at the one autojump suggests
+r() {
+  if [ "$1" != "" ]; then
+    if [ -d "$1" ]; then
+      ranger "$1"
+    elif [ -f "$1" ]; then
+      ranger --selectfile="$1"
+    else
+      out="$(autojump $1)"
+      if [ -d "$out" ]; then
+        ranger "$out"
+      else
+        ranger --selectfile="$out"
+      fi
+    fi
+  else
+    ranger
+  fi
+	return $?
+}
+
 
