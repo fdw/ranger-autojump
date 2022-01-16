ea00b2d2b1a70a515accf98ad98bf70134d62b8e
Charles N. Christensen <charles.n.chr@gmail.com>
2022-01-16 18:19:49 +0000

    Antigen compatibility

---
+++ r_function.zsh
@@ -0,0 +1,22 @@
+# Add a `r` function to zsh that opens ranger either at the given directory or
+# at the one autojump suggests
+r() {
+  if [ "$1" != "" ]; then
+    if [ -d "$1" ]; then
+      ranger "$1"
+    elif [ -f "$1" ]; then
+      ranger --selectfile="$1"
+    else
+      out="$(autojump $1)"
+      if [ -d "$out" ]; then
+        ranger "$out"
+      else
+        ranger --selectfile="$out"
+      fi
+    fi
+  else
+    ranger
+  fi
+	return $?
+}
+
